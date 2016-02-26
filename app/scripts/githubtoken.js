0577606734c05e70d400a400d33dc94adc7495ec
Joshua Abu <joshujah007@gmail.com>
2016-02-25 20:52:55 -0500

    sidebar update

---
+++ app/scripts/githubtoken.js
@@ -0,0 +1,1 @@
+module.exports = {'token': "71edcc356edb48f733688b347b05fa1289bd1e8e"};
---
+++ app/scripts/index.js
@@ -0,0 +1,43 @@
+var $ = require('jquery');
+var _ = require('underscore');
+
+var handlebars = require('handlebars');
+var githubtoken = require('./githubtoken.js').token;
+var username = "Absolutestunna";
+
+
+var source = $("#sidebar_info").html();
+var template = handlebars.compile(source);
+
+
+var url = "https://api.github.com/users/" + username;
+if(typeof(githubtoken) !== "undefined"){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken,
+    }
+  });
+}
+$.ajax(url).then(function(data){
+  console.log(data)
+  var context = {
+     name: data.name,
+     avatar_url: data.avatar_url,
+      blog: data.blog,
+      location: data.location,
+      email: data.email,
+      html_url: data.html_url,
+      login: data.login,
+      location: data.location,
+      blog: data.blog,
+      followersNum: data.followers,
+      followers: data.followers_url,
+      followingNum: data.following,
+      following: data.following_url,
+      starred: data.starred_url,
+      date: data.created_at,
+      organizations: data.organizations,
+    }
+    $(".sidebar").append(template(context))
+
+});
